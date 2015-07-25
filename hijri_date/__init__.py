c9fda08f50451fda8e7e2525a750dc35189e41ee
Murtaza Gulamali <mygulamali@gmail.com>
2015-07-25 18:06:05 +0100

    Return number of days in specific month for specific year.

--- hijri_date/__init__.py
+++ hijri_date/__init__.py
@@ -13,3 +13,13 @@ class HijriDate(object):
             if (year % 30 == i):
                 return True
         return False
+
+    def days_in_month(self, month=None, year=None):
+        if month == None:
+            month = self.month
+        if year == None:
+            year = self.year
+
+        if (month == 12 and self.is_kabisa(year)) or (month % 2 == 1):
+            return 30
+        return 29
--- tests/test_hijri_date.py
+++ tests/test_hijri_date.py
@@ -8,3 +8,8 @@ class TestHijriDate:
     def test_kabisa_year(self):
         assert not self.date.is_kabisa()
         assert self.date.is_kabisa(1431)
+
+    def test_days_in_month(self):
+        assert self.date.days_in_month() == 29
+        assert self.date.days_in_month(12) == 29
+        assert self.date.days_in_month(12, 1431) == 30
