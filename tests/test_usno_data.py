7208deac093e7979af74cc4296b36e792017454c
Murtaza Gulamali <mygulamali@gmail.com>
2015-07-26 14:03:03 +0100

    Refactor USNO_Data test.

--- tests/test_usno_data.py
+++ tests/test_usno_data.py
@@ -5,7 +5,10 @@ def assert_approximate(expected_value, observed_value, tolerance=1.0e-12):
     assert abs(expected_value - observed_value) < tolerance
 
 class TestUSNO_Data:
+    def setup(self):
+        self.lat = 51.0 + 32.0/60.0
+
     def test_angle_components(self):
-        (degrees, minutes) = USNO_Data.angle_components(51.0 + 32.0/60.0)
+        (degrees, minutes) = USNO_Data.angle_components(self.lat)
         assert_approximate(51, degrees)
         assert_approximate(32, minutes)
