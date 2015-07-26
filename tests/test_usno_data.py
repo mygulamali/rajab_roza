7080733bc0c1db77fe57726687306cc1b596dab0
Murtaza Gulamali <mygulamali@gmail.com>
2015-07-26 14:21:32 +0100

    Return parameters for request to USNO website.

--- tests/test_usno_data.py
+++ tests/test_usno_data.py
@@ -4,9 +4,30 @@ from usno_data import USNO_Data
 class TestUSNO_Data:
     def setup(self):
         self.lat = 51.0 + 32.0/60.0
+        self.lng = -22.0/60.0
+        self.year = 2015
+        self.usno_data = USNO_Data(self.lat, self.lng)
 
     def test_angle_components(self):
         (direction, degrees, minutes) = USNO_Data.angle_components(self.lat)
         assert direction == 1
         assert degrees == 51
         assert minutes == 32
+
+    def test_parameters(self):
+        expected_parameters = {
+            "FFX": 2,
+            "xxy": self.year,
+            "tabtyb": 0,
+            "place": "",
+            "xx0": -1,
+            "xx1": 0,
+            "xx2": 22,
+            "yy0": 1,
+            "yy1": 51,
+            "yy2": 32,
+            "zz0": 1,
+            "zz1": 0,
+            "ZZZ": "END"
+        }
+        assert self.usno_data.parameters(self.year) == expected_parameters
--- usno_data/__init__.py
+++ usno_data/__init__.py
@@ -9,3 +9,26 @@ class USNO_Data:
         degrees = int(angle)
         minutes = round((angle - degrees)*60)
         return (direction, abs(degrees), abs(minutes))
+
+    def parameters(self, year):
+        (lat_direction, lat_degrees, lat_minutes) = USNO_Data.angle_components(self.lat)
+        (lng_direction, lng_degrees, lng_minutes) = USNO_Data.angle_components(self.lng)
+
+        return {
+            "FFX": 2,
+            "xxy": year,
+            "tabtyb": 0, # table of sunrise/sunset times
+            "place": "",
+            # longitude
+            "xx0": lng_direction, # west = -1, east = 1
+            "xx1": lng_degrees,
+            "xx2": lng_minutes,
+            # latitude
+            "yy0": lat_direction, # south = -1, north = 1
+            "yy1": lat_degrees,
+            "yy2": lat_minutes,
+            # timezone
+            "zz0": 1, # west = -1, east = 1
+            "zz1": 0, # UTC
+            "ZZZ": "END"
+        }
