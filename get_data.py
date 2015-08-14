633384674a97e8813970e6b19ce146289346a5e2
Murtaza Gulamali <mygulamali@gmail.com>
2015-08-14 11:04:01 +0100

    Get data for London from 1400H-1500H, inclusive.

--- get_data.py
+++ get_data.py
@@ -4,9 +4,9 @@ from rajab_roza import RajabRoza
 
 lat = 51.0 + 32.0/60.0
 lng = -22.0/60.0
-start_year = 1435
-end_year = 1436
-filename = "1435-1436.yml"
+start_year = 1400
+end_year = 1500
+filename = "london-durations.yml"
 
 if __name__ == '__main__':
     rajab_roza = RajabRoza(lat, lng, start_year, end_year)
